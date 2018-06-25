5524415f7997b05fef2d7917d0d68cdd1ee93d7d
e <644583255@qq.com>
2018-06-25 13:55:43 +0800

    add

--- pizzas/models.py
+++ pizzas/models.py
@@ -10,4 +10,12 @@ class Pizza(models.Model):
 
     def __str__(self):
         """返回模型字符串"""
-        return self.text
+        return self.name
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
