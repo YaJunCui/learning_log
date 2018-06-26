b6af57ace65f8ef79f75c1ca3fbd28194720735d
e <644583255@qq.com>
2018-06-26 18:48:23 +0800

    add

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -19,5 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('learning_logs.urls', namespace='learning_logs')),
-    url(r'^pizza/', include('pizzas.urls', namespace='pizzas')),
 ]
