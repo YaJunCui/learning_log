349f35739cd2163a6e04f20bbd85ccf5fc7dabfa
e <644583255@qq.com>
2018-06-29 19:35:39 +0800

    add

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,10 @@ from . import views
 urlpatterns = [
     # 登录页面
     url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
-    
+
+    # 注销
+    url(r'^logout/$', views.logout_view, name='logout'),
+
+    # 注册页面
+    url(r'^register/$', views.register, name='register'),
 ]
